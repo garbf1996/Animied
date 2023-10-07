f7589a24c4edcf20fbacc0b5c6010c5a8866a839
garber <garbfbatista@gmaail.com>
2023-10-07 00:00:46 -0400

    [UI:creacion de menu hamburguesas ]

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header className='bg-slate-700 w-full'>
       <nav className='flex  justify-between md:justify-between items-center h-16 mx-auto md:px-32 p-7'>
@@ -9,32 +14,8 @@ const Header = () => {
           alt='logo'
           className='w-36 '
         />
-        <button
-          data-collapse-toggle='navbar-default'
-          type='button'
-          className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-          aria-controls='navbar-default'
-          aria-expanded='false'
-        >
-          <span className='sr-only'>Open main menu</span>
-          <svg
-            className='w-5 h-5'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 17 14'
-          >
-            <path
-              stroke='currentColor'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
-              d='M1 1h15M1 7h15M1 13h15'
-            />
-          </svg>
-        </button>
-        <div className='hidden'>
-          <ul className='md:flex items-center gap-4 font-poppins  text-xl text-white'>
+        <div className={`md:flex ${isMenuOpen ? "flex" : "hidden"}`}>
+          <ul className='md:flex  items-center gap-4 font-poppins  text-xl text-white'>
             <li>
               <a
                 className='hover:scale-105 hover:text-blue-500 duration-150'
@@ -61,6 +42,52 @@ const Header = () => {
             </li>
           </ul>
         </div>
+
+        <button
+          onClick={toggleMenu}
+          data-collapse-toggle='navbar-default'
+          type='button'
+          className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+          aria-controls='navbar-default'
+          aria-expanded='false'
+        >
+          {isMenuOpen ? (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='w-full h-auto text-blue-500'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              strokeWidth='2'
+              stroke='currentColor'
+              fill='none'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+              <path d='M18 6l-12 12'></path>
+              <path d='M6 6l12 12'></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='w-full h-auto text-blue-500'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              strokeWidth='2'
+              stroke='currentColor'
+              fill='none'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+              <path d='M4 6l16 0'></path>
+              <path d='M4 12l16 0'></path>
+              <path d='M4 18l16 0'></path>
+            </svg>
+          )}
+        </button>
       </nav>
     </header>
   );
